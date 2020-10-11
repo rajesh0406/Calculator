e4feee0ac5a0c4a7bee49afc5aa0f330b9b6d658
Rajesh <rajgan8428800171@gmail.com>
2020-10-11 22:31:28 +0530

    Updates

---
+++ precache-manifest.535414af50f67a97302f99f9647fc3ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f86c0b0208faf511b9cdbf46b83864f",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "59f297eb3f6039f7ee46",
+    "url": "/Calculator/static/css/main.8455b03e.chunk.css"
+  },
+  {
+    "revision": "83c708f55d771b07501a",
+    "url": "/Calculator/static/js/2.028d59cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.028d59cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f297eb3f6039f7ee46",
+    "url": "/Calculator/static/js/main.410e3b99.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
